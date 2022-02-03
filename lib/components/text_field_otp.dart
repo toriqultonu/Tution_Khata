be9f79b10a6e9eff40b46da3043d523e329b36a4
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-03 17:53:28 +0600

    text field otp updated

--- lib/components/text_field_otp.dart
+++ lib/components/text_field_otp.dart
@@ -1,1 +1,41 @@
 import 'package:flutter/material.dart';
+
+class TextFieldOtp extends StatelessWidget {
+
+  final bool first, last;
+
+  const TextFieldOtp({Key? key, required this.first, required this.last}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 85,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: TextField(
+          autofocus: true,
+          onChanged: (value){
+
+          },
+          showCursor: false,
+          readOnly: false,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          keyboardType: TextInputType.number,
+          maxLength: 1,
+          decoration: InputDecoration(
+            counter: Offstage(),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.black12),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.purple),
+              borderRadius: BorderRadius.circular(12),
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
