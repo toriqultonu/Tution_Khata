f4caa0ca1127b17a9301540a48060bb22b0b7644
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-03 20:35:38 +0600

    text field otp customized

--- lib/components/text_field_otp.dart
+++ lib/components/text_field_otp.dart
@@ -9,35 +9,48 @@ class TextFieldOtp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 85,
+      width: 56,
+      height: 47,
       child: AspectRatio(
         aspectRatio: 1.0,
-        child: TextField(
-          autofocus: true,
-          onChanged: (value){
-            if(value.length == 1 && last == false){
-              FocusScope.of(context).nextFocus();
-            }
-            if(value.length == 1 && first == false){
-              FocusScope.of(context).previousFocus();
-            }
-          },
-          showCursor: false,
-          readOnly: false,
-          textAlign: TextAlign.center,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-          keyboardType: TextInputType.number,
-          maxLength: 1,
-          decoration: InputDecoration(
-            counter: Offstage(),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(width: 2, color: Colors.black12),
-              borderRadius: BorderRadius.circular(12),
+        child: Container(
+          width: 56,
+          height: 47,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(6.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.8),
+                  spreadRadius: 0.1,
+                  blurRadius: 2,
+                  offset: Offset(3, 3),
+                )
+              ]),
+          child: TextField(
+            autofocus: true,
+            onChanged: (value){
+              if(value.length == 1 && last == false){
+                FocusScope.of(context).nextFocus();
+              }
+              if(value.length == 1 && first == false){
+                FocusScope.of(context).previousFocus();
+              }
+            },
+            showCursor: false,
+            readOnly: false,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            keyboardType: TextInputType.number,
+            maxLength: 1,
+            decoration: InputDecoration(
+              counter: Offstage(),
+              border: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              errorBorder: InputBorder.none,
+              disabledBorder: InputBorder.none,
             ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(width: 2, color: Colors.purple),
-              borderRadius: BorderRadius.circular(12),
-            )
           ),
         ),
       ),
