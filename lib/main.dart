7706efca4b2b53ba173a76deb22f2e3af3043932
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-06 12:17:21 +0600

    home locatin added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tution_khata/constant.dart';
+import 'package:tution_khata/views/home_page.dart';
 import 'package:tution_khata/views/login_page.dart';
 import 'package:tution_khata/views/phone_verification_page.dart';
 import 'package:tution_khata/views/register_page.dart';
@@ -28,6 +29,8 @@ class MyApp extends StatelessWidget {
         LoginScreen.id: (context) => LoginScreen(),
         RegisterScreen.id: (context) => RegisterScreen(),
         PhoneVerification.id: (context) => PhoneVerification(),
+        HomeScreen.id: (context) => HomeScreen(),
+
       },
 
       home: Scaffold(
