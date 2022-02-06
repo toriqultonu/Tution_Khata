3b8120ca03eee36dd9cec58e4635e1c16961f50b
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-06 15:49:20 +0600

    home page appbar started

--- lib/components/home_page_app_bar.dart
+++ lib/components/home_page_app_bar.dart
@@ -6,7 +6,22 @@ class HomeAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      height: 74,
+      width: ,
+      child: Row(
+        children: [
+          Container(
+            width: 48.0,
+            height: 48.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  image: AssetImage("assets/component/profiledemo.png"),
+                  fit: BoxFit.cover),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
