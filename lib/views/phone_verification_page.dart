db1fb2bdd580f730a8683fe41e8c9040c5cb84a1
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-06 11:41:54 +0600

    image issue resolved

--- lib/views/phone_verification_page.dart
+++ lib/views/phone_verification_page.dart
@@ -24,51 +24,67 @@ class _PhoneVerificationState extends State<PhoneVerification> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: primaryColor),
         ),
-        body: Center(
-          child: Container(
-            child: Column(
-              children: [
-                SizedBox(height: 34,),
-                Container(
-                  padding: EdgeInsets.only(top: 18,left: 23, right: 23, bottom: 20),
-                  height: 143,
-                  width: 333,
-                  decoration: BoxDecoration(color: boxColor, boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.8),
-                      spreadRadius: 0.3,
-                      blurRadius: 6,
-                      offset: Offset(4, 5),
-                    )
-                  ]),
-                  child: Column(
-                    children: [
-                      Container(child: Text('Please enter the 4 digit that have sent to your phone', textAlign: TextAlign.center,style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500,),),),
-                      Spacer(),
-                      Row(
-                        children: [
-                          TextFieldOtp(first: true, last: false),
-                          Spacer(),
-                          TextFieldOtp(first: true, last: false),
-                          Spacer(),
-                          TextFieldOtp(first: true, last: false),
-                          Spacer(),
-                          TextFieldOtp(first: true, last: false),
-                        ],
-                      ),
-                    ],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              child: Column(
+                children: [
+                  SizedBox(height: 34,),
+                  Container(
+                    width: 279,
+                    height: 300,
+                    margin: EdgeInsets.only(bottom: 25),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(5.0),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/component/rafiki.png"),
+                        )
+                    ),
                   ),
-                ),
-                SizedBox(height: 20,),
-                RoundedButton(color: primaryColor, title: 'Verify Now', onPressed: () {
-                  print('verify');
-                }, height: 45, width: 277),
-                SizedBox(height: 25,),
-                Container(
-                  child: Text('Wait till sending your code again ', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),),
-                )
-              ],
+                  Container(
+                    padding: EdgeInsets.only(top: 18,left: 23, right: 23, bottom: 20),
+                    height: 143,
+                    width: 333,
+                    decoration: BoxDecoration(color: boxColor, boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.8),
+                        spreadRadius: 0.3,
+                        blurRadius: 6,
+                        offset: Offset(4, 5),
+                      )
+                    ]),
+                    child: Column(
+                      children: [
+                        Container(child: Text('Please enter the 4 digit that have sent to your phone', textAlign: TextAlign.center,style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500,),),),
+                        Spacer(),
+                        Row(
+                          children: [
+                            TextFieldOtp(first: true, last: false),
+                            Spacer(),
+                            TextFieldOtp(first: true, last: false),
+                            Spacer(),
+                            TextFieldOtp(first: true, last: false),
+                            Spacer(),
+                            TextFieldOtp(first: true, last: false),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  RoundedButton(color: primaryColor, title: 'Verify Now', onPressed: () {
+                    print('verify');
+                  }, height: 45, width: 277),
+                  SizedBox(height: 25,),
+                  Container(
+                    child: Text('Wait till sending your code again ', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),),
+                  )
+                ],
+              ),
             ),
           ),
         ),
