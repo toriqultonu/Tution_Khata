7880e91e1e94f95ec53af663c1b1e8ea489ca655
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-06 12:39:51 +0600

    home app bar created

---
+++ lib/components/home_page_app_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
