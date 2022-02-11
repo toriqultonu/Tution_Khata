4e840df5d7e89bb137a638c628274d7d0389cc5e
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-11 20:42:24 +0600

    student_card.dart updated

--- lib/components/student_card.dart
+++ lib/components/student_card.dart
@@ -3,49 +3,51 @@ import 'package:tution_khata/constant.dart';
 import 'package:tution_khata/views/batch_details.dart';
 
 class StudentCard extends StatelessWidget {
-  final String batchName;
+  final String studentName;
   final String date;
   final String time;
   final String batchId;
-  StudentCard({ required this.batchName, required this.date, required this.batchId, required this.time});
+  StudentCard({ required this.studentName, required this.date, required this.batchId, required this.time});
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        Navigator.pushNamed(context, BatchDetails.id);
-      },
-      child: Container(
-        margin: EdgeInsets.only(bottom: 8),
-        alignment: Alignment.center,
-        child: Stack(
-          children: [
-            Container(
-              height: 50,
-              width: MediaQuery.of(context).size.width * 0.9,
-              decoration: CustomBoxDecoration(Colors.white),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(batchName, style: TextStyle(color: primaryColor, fontSize: 20, fontWeight: FontWeight.w500),),
-                      SizedBox(height: 5,),
-                      Text('Phone: 2398472304', style: TextStyle( fontSize: 14),),
-                      SizedBox(height: 3,),
-                    ],
-                  ),
-                  Container(
+    return Container(
+      margin: EdgeInsets.only(bottom: 8),
+      alignment: Alignment.center,
+      child: Stack(
+        children: [
+          Container(
+            height: 50,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: CustomBoxDecoration(Colors.white),
+            child: Row(
+              children: [
+                SizedBox(width: 34,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(studentName, style: TextStyle(color: primaryColor, fontSize: 18, fontWeight: FontWeight.w500),),
+                    SizedBox(height: 5,),
+                    Text('Phone: 2398472304', style: TextStyle( fontSize: 14),),
+                    SizedBox(height: 3,),
+                  ],
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: (){
+                    print('settings');
+                  },
+                  child: Container(
                     alignment: Alignment.centerRight,
-                    padding: EdgeInsets.only(right: 5),
-                    child: Icon(Icons.chair),
-                  )
-                ],
-              ),
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.settings),
+                  ),
+                )
+              ],
+            ),
 
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
