83c13065881c8097c941bc235a1326f32634adc0
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-11 19:26:57 +0600

    basic design done

--- lib/views/batch_details.dart
+++ lib/views/batch_details.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:tution_khata/components/custom_app_bar.dart';
 import 'package:tution_khata/constant.dart';
 
-void main() {
-  runApp(BatchDetails());
-}
+// void main() {
+//   runApp(BatchDetails());
+// }
 
 
 class BatchDetails extends StatefulWidget {
@@ -24,7 +25,7 @@ class _BatchDetailsState extends State<BatchDetails> {
           backgroundColor: primaryColor,
           title: CustomAppBar(str1: 'Batch Details', str2: '',),
           centerTitle: true,
-          elevation: 13,
+          elevation: 15,
         ),
         body: Container(
 
@@ -34,10 +35,108 @@ class _BatchDetailsState extends State<BatchDetails> {
               child: Column(
                 children: [
                   Container(
-                    height: 100,
+                    padding: EdgeInsets.only(left: 29),
+                    height: 110,
                     decoration: CustomBoxDecoration(boxColor),
-
-                  )
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.topRight,
+                          padding: EdgeInsets.only(top: 8, right: 14),
+                          child: Text('Batch Code: 010101', style: TextStyle(fontWeight: FontWeight.w600,),),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Batch Name: Math 1', style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.w600, fontSize: 17),),
+                        ),
+                        SizedBox(height: 5,),
+                        Container(alignment: Alignment.centerLeft,child: Text('Day: '),),
+                        SizedBox(height: 5,),
+                        Container(alignment: Alignment.centerLeft,child: Text('Time: '),),
+                        GestureDetector(
+                          onTap: (){
+                            print('click to edit');
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(right:14 ),
+                            alignment: Alignment.bottomRight,
+                            child: Text('Click to edit', style: TextStyle(color: Colors.redAccent),),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Container(
+                    padding: EdgeInsets.only(left: 29, top: 11),
+                    height: 100,
+                    decoration: CustomBoxDecoration(primaryColor),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 55.0,
+                              height: 55.0,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    image: AssetImage("assets/component/profiledemo.png"),
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
+                            SizedBox(width: 16,),
+                            Container(
+                              height: 60,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    child: Text('Name: Mojura Moula', style: TextStyle(fontFamily: 'Nunito Sans', fontWeight: FontWeight.w600, fontSize: 16, color: Colors.white),),
+                                  ),
+                                  Spacer(),
+                                  Container(
+                                    child: Text('Role: Captain', style: TextStyle(fontFamily: 'Nunito', fontWeight: FontWeight.w400, fontSize: 14, color: Colors.white),),
+                                  ),
+                                  Spacer(),
+                                  Container(
+                                    child: Text('Phone no: 01699999999', style: TextStyle(fontFamily: 'Nunito', fontWeight: FontWeight.w400, fontSize: 14, color: Colors.white),),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                          onTap: (){
+                            print('click to edit');
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(right:14, bottom: 5),
+                            alignment: Alignment.bottomRight,
+                            child: Text('Click to edit', style: TextStyle(color: Colors.redAccent),),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Expanded(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.only(top: 10),
+                      decoration: CustomBoxDecoration(boxColor),
+                      child: Column(
+                        children: [
+                          Container(
+                            child: Text('Student List', style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.w600, fontSize: 17),),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,)
                 ],
               ),
             ),
