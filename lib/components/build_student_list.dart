32c2fff6ccd6f0f6d468b5ee9751683a341d5489
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-12 20:31:10 +0600

    new function added

--- lib/components/build_student_list.dart
+++ lib/components/build_student_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tution_khata/components/student_card.dart';
 
+import 'attendance_card.dart';
+
 Widget buildStudentList(AsyncSnapshot snapshot){
   return ListView.builder(
       physics: ClampingScrollPhysics(),
@@ -12,5 +14,22 @@ Widget buildStudentList(AsyncSnapshot snapshot){
             batchId: '3243',
             time: '4.00-5.00');
       }
+  );
+}
+
+
+Widget buildAttendanceList(AsyncSnapshot snapshot){
+  return Container(
+    child: ListView.builder(
+        physics: ClampingScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: snapshot.data.length,
+        itemBuilder: (context, index) {
+          return AttendanceCard(studentName: snapshot.data[index].batchName,
+              date: 'sat-sun-mon',
+              batchId: '3243',
+              time: '4.00-5.00');
+        }
+    ),
   );
 }
