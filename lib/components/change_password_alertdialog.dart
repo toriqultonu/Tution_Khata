d3ca98c9f4c5467a50d2f545dfad0cfbd440ca4b
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-16 11:18:47 +0600

    change password update

--- lib/components/change_password_alertdialog.dart
+++ lib/components/change_password_alertdialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tution_khata/components/custom_password_filed.dart';
 import 'package:tution_khata/components/rounded_button.dart';
 import 'package:tution_khata/components/textformfield_design.dart';
 import 'package:tution_khata/views/phone_verification_page.dart';
@@ -12,7 +13,7 @@ class ChangePasswordAlert extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.all(20),
-        width: MediaQuery.of(context).size.width *85,
+        //width: MediaQuery.of(context).size.width *.9,
         height: 350,
         //color: boxColor,
         decoration: BoxDecoration(color: boxColor),
@@ -20,18 +21,15 @@ class ChangePasswordAlert extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Center(
-              child: Text('Change Your Password', style: TextStyle(fontFamily: 'Raleway', fontSize: 17, fontWeight: FontWeight.w500),),
+              child: Text('Change Your Password', style: TextStyle(fontFamily: 'Raleway', fontSize: 18, fontWeight: FontWeight.w500),),
             ),
 
             SizedBox(height: 31,),
-            CustomTextFormField(
-                'Phone No.', Icon(Icons.phone), TextInputType.phone),
+            CustomPasswordField(labelText: 'Current Password'),
             Spacer(),
-            CustomTextFormField(
-                'Phone No.', Icon(Icons.phone), TextInputType.phone),
+            CustomPasswordField(labelText: 'New Password'),
             Spacer(),
-            CustomTextFormField(
-                'Phone No.', Icon(Icons.phone), TextInputType.phone),
+            CustomPasswordField(labelText: 'Re-Enter Password'),
             Spacer(),
             RoundedButton(
                 color: primaryColor,
