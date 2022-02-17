b5018f717b58cd486913708372654ae8e5d2761e
toriqultonu22 <toriqultonu22@gmail.com>
2022-02-17 17:42:35 +0600

    created

---
+++ lib/components/month_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+
+class MonthButton extends StatefulWidget {
+  final String label;
+  const MonthButton({Key? key, required this.label}) : super(key: key);
+
+  @override
+  _MonthButtonState createState() => _MonthButtonState();
+}
+
+class _MonthButtonState extends State<MonthButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 72,
+        height: 25,
+        decoration: CustomBoxDecoration(Colors.redAccent),
+        child: Center(child: Text(widget.label, style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w400),))
+    );
+  }
+}
