ec4ddb3e9f796e4946318dc2a815b45b5f6eb5cb
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-01 11:51:06 +0600

    created

---
+++ lib/views/captain_panel_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CaptainPanel extends StatefulWidget {
+  const CaptainPanel({Key? key}) : super(key: key);
+
+  @override
+  _CaptainPanelState createState() => _CaptainPanelState();
+}
+
+class _CaptainPanelState extends State<CaptainPanel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
