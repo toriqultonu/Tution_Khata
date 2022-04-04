233ea4fe984546a45ee7b352d2e4b3067e014bed
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-04 16:00:15 +0600

    design changed

--- lib/views/add_student_view.dart
+++ lib/views/add_student_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tution_khata/components/rounded_button.dart';
 
 import '../components/custom_app_bar.dart';
 import '../constant.dart';
@@ -11,8 +12,19 @@ class AddStudent extends StatefulWidget {
 }
 
 class _AddStudentState extends State<AddStudent> {
+
+  String? selectedValue;
+  int? selectedRadio;
+
+  setSelectedRadio(val){
+    setState(() {
+      selectedRadio = val;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.white),
@@ -27,11 +39,135 @@ class _AddStudentState extends State<AddStudent> {
             children: [
               Container(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.width * 0.9,
                 padding: EdgeInsets.all(20),
                 decoration: CustomBoxDecoration(secondaryColor),
                 child: Column(
                   children: [
+                      Row(
+                        children: [
+                          RoundedButton(color: primaryColor, title: 'Add Student', onPressed: (){}, height: 30, width: 15),
+                          Spacer(),
+                          RoundedButton(color: primaryColor, title: 'Add Student', onPressed: (){}, height: 30, width: 15),
+
+                        ],
+                      ),
+                    Spacer(),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(6.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.8),
+                              spreadRadius: 0.1,
+                              blurRadius: 2,
+                              offset: Offset(3, 3),
+                            )
+                          ]),
+                      child: TextFormField(
+                        textAlignVertical: TextAlignVertical.center,
+                        keyboardType: TextInputType.name,
+                        decoration: InputDecoration(
+                          isCollapsed: true,
+                          prefixIcon: Icon(Icons.person),
+                          labelText: 'Full Name',
+                          hintStyle: hintText,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                        ),
+                        onChanged: (value){
+
+                        },
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(6.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.8),
+                              spreadRadius: 0.1,
+                              blurRadius: 2,
+                              offset: Offset(3, 3),
+                            )
+                          ]),
+                      child: TextFormField(
+                        textAlignVertical: TextAlignVertical.center,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          isCollapsed: true,
+                          prefixIcon: Icon(Icons.phone),
+                          labelText: 'Phone No.',
+                          hintStyle: hintText,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                        ),
+                        onChanged: (value){
+
+                        },
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+
+                      width: MediaQuery.of(context).size.width,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(6.0),
+
+                          boxShadow: [BoxShadow(
+                            color: Colors.grey.withOpacity(0.8),
+                            spreadRadius: 0.1,
+                            blurRadius: 2,
+                            offset: Offset(3,3),
+                          )
+                          ]
+                      ),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                          children:[ Radio(
+                            value: 1,
+                            groupValue: selectedRadio,
+                            activeColor: primaryColor,
+                            onChanged: (value){
+                              setSelectedRadio(value);
+                              print(selectedRadio);
+                            },
+                          ),
+                            Text('Male', style: TextStyle(color: hintTextColor, fontWeight: FontWeight.w300, fontSize: 14),),
+                            SizedBox(width: 54,),
+                            Radio(
+                              value: 2,
+                              groupValue: selectedRadio,
+                              activeColor: primaryColor,
+                              onChanged: (value){
+                                setSelectedRadio(value);
+                                print(selectedRadio);
+                              },
+                            ),
+                            Text('Female', style: TextStyle(color: hintTextColor, fontWeight: FontWeight.w300, fontSize: 14),),
+                          ]
+                      ),
+                    ),
+                    Spacer(),
+                    RoundedButton(color: primaryColor, title: 'Apply Admission', onPressed: (){}, height: 30, width: 15),
 
                   ],
                 ),
