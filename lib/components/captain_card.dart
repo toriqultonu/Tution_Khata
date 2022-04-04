bd9ae90408e17e935b4e692da472a97569db891e
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-04 16:28:25 +0600

    update

--- lib/components/captain_card.dart
+++ lib/components/captain_card.dart
@@ -28,37 +28,29 @@ class _CaptainCardState extends State<CaptainCard> {
             onTap: (){
             },
             child: Container(
-              height: 63,
+              margin: EdgeInsets.only(bottom: 5),
+              height: 65,
               width: MediaQuery.of(context).size.width * 0.98,
-              decoration: CustomBoxDecoration(secondaryColor),
+              decoration: CustomBoxDecoration(boxColor),
               child: Row(
                 children: [
-                  SizedBox(width: 34,),
+                  SizedBox(width: 20,),
                   Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Batch Id: ${widget.batchId}", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),),
                       Spacer(),
-                      Text("Captain Name: ${widget.captainName}", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),),
+                      Text("Batch Id: ${widget.batchId}", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),),
                       Spacer(),
-                      Text("Phone: ${widget.phone}", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),),
+                      Text("Captain Name: ${widget.captainName}", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),),
+                      Spacer(),
+                      Text("Phone: ${widget.phone}", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),),
+                      Spacer(),
                     ],
                   ),
                   Spacer(),
-                  // Container(
-                  //   child: CheckboxListTile(
-                  //     //controlAffinity: ListTileControlAffinity.leading,
-                  //     value: widget.isChecked,
-                  //     onChanged: (value){
-                  //       setState(() {
-                  //         widget.isChecked = value!;
-                  //       });
-                  //     },
-                  //     activeColor: primaryColor,
-                  //     checkColor: Colors.white,
-                  //   ),
-                  // )
+                  Icon(Icons.edit),
+                  SizedBox(width: 20,),
                 ],
               ),
 
