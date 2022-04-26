5477a53b05aa8a11a480dd8777b5d91005087ff1
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-26 16:01:49 +0600

    time picker update

--- lib/views/batch_update.dart
+++ lib/views/batch_update.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:day_picker/day_picker.dart';
+import 'package:day_picker/model/day_in_week.dart';
 import 'package:flutter/material.dart';
 import 'package:tution_khata/build_lists/build_unapprovedStudent_list.dart';
 import 'package:tution_khata/components/buttons/custom_flat_button.dart';
@@ -28,6 +32,77 @@ class _BatchUpdateState extends State<BatchUpdate> {
     });
   }
 
+  List<DayInWeek> _days = [
+    DayInWeek(
+      "Sat",
+    ),
+    DayInWeek(
+      "Sun",
+    ),
+    DayInWeek(
+      "Mon",
+    ),
+    DayInWeek(
+      "Tue",
+    ),
+    DayInWeek(
+      "Wed",
+    ),
+    DayInWeek(
+      "Thu",
+    ),
+    DayInWeek(
+      "Fri",
+    ),
+  ];
+
+  TimeOfDay? startTime;
+  TimeOfDay? endTime;
+
+  Future pickStartTime(BuildContext context) async{
+    final initialTime = TimeOfDay(hour: 9, minute: 0);
+    final newTime = await showTimePicker(context: context, initialTime: startTime ?? initialTime);
+
+    if(newTime == null) return;
+
+    setState(() {
+      startTime = newTime;
+    });
+  }
+
+  Future pickEndTime(BuildContext context) async{
+    final initialTime = TimeOfDay(hour: 9, minute: 0);
+    final newTime = await showTimePicker(context: context, initialTime: startTime ?? initialTime);
+
+    if(newTime == null) return;
+
+    setState(() {
+      endTime = newTime;
+    });
+  }
+
+  getStartTime(){
+    if(startTime == null){
+      return 'Select Start Time';
+    }
+    else{
+      final hour = startTime?.hour.toString().padLeft(2,'0');
+      final minute = startTime?.minute.toString().padLeft(2,'0');
+      return 'Start Time: ${startTime?.hour}:${startTime?.minute}';
+    }
+  }
+
+  getEndTime(){
+    if(endTime == null){
+      return 'Select End Time';
+    }
+    else{
+      final hour = endTime?.hour.toString().padLeft(2,'0');
+      final minute = endTime?.minute.toString().padLeft(2,'0');
+      return 'End Time: ${endTime?.hour}:${endTime?.minute}';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -48,7 +123,7 @@ class _BatchUpdateState extends State<BatchUpdate> {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.width*1.2,
                 margin: EdgeInsets.all(15),
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(15),
                 decoration: CustomBoxDecoration(boxColor),
                 child: Column(
                   children: [
@@ -88,108 +163,59 @@ class _BatchUpdateState extends State<BatchUpdate> {
                       ),
                     ),
                     Spacer(),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 45,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(6.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.8),
-                              spreadRadius: 0.1,
-                              blurRadius: 2,
-                              offset: Offset(3, 3),
-                            )
-                          ]),
-                      child: TextFormField(
-                        textAlignVertical: TextAlignVertical.center,
-                        keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          isCollapsed: true,
-                          prefixIcon: Icon(Icons.phone),
-                          labelText: 'Days',
-                          hintStyle: hintText,
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                        ),
-                        onChanged: (value){
+                    Text('Select Days:'),
+                    SelectWeekDays(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      days: _days,
+                      boxDecoration: CustomBoxDecoration(primaryColor),
+                      onSelect: (values) {
+                        log('$values');
+                      },
+                    ),
+                    Spacer(),
 
-                        },
+                    GestureDetector(
+                      onTap: (){
+                        pickStartTime(context);
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 45,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.8),
+                                spreadRadius: 0.1,
+                                blurRadius: 2,
+                                offset: Offset(3, 3),
+                              )
+                            ]),
+                        child: Center(child: Text(getStartTime())),
                       ),
                     ),
                     Spacer(),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 45,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(6.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.8),
-                              spreadRadius: 0.1,
-                              blurRadius: 2,
-                              offset: Offset(3, 3),
-                            )
-                          ]),
-                      child: TextFormField(
-                        textAlignVertical: TextAlignVertical.center,
-                        keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          isCollapsed: true,
-                          prefixIcon: Icon(Icons.phone),
-                          labelText: 'Start Time',
-                          hintStyle: hintText,
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                        ),
-                        onChanged: (value){
-
-                        },
-                      ),
-                    ),
-                    Spacer(),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 45,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(6.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.8),
-                              spreadRadius: 0.1,
-                              blurRadius: 2,
-                              offset: Offset(3, 3),
-                            )
-                          ]),
-                      child: TextFormField(
-                        textAlignVertical: TextAlignVertical.center,
-                        keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          isCollapsed: true,
-                          prefixIcon: Icon(Icons.phone),
-                          labelText: 'End Time',
-                          hintStyle: hintText,
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                        ),
-                        onChanged: (value){
-
-                        },
+                    GestureDetector(
+                      onTap: (){
+                        pickEndTime(context);
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 45,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.8),
+                                spreadRadius: 0.1,
+                                blurRadius: 2,
+                                offset: Offset(3, 3),
+                              )
+                            ]),
+                        child: Center(child: Text(getEndTime())),
                       ),
                     ),
                     Spacer(),
--- lib/views/test2.dart
+++ lib/views/test2.dart
@@ -1,14 +1,9 @@
+import 'dart:developer';
+
+import 'package:day_picker/day_picker.dart';
+import 'package:day_picker/model/day_in_week.dart';
 import 'package:flutter/material.dart';
 import 'package:tution_khata/constant.dart';
-
-class ContactModel{
-
-  String name, phoneNumber;
-  bool isSelected;
-
-  ContactModel(this.name, this.phoneNumber, this.isSelected);
-
-}
 
 void main() {
   runApp(MyApp());
@@ -18,137 +13,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<ContactModel> contacts = [
-    ContactModel("Zaman", "0778979454", false),
-    ContactModel("Naim", "0766223795", false),
-    ContactModel("Sardar", "0749112016", false),
-    ContactModel("Baqer", "0775286570", false),
-    ContactModel("Yasin", "0744795640", false),
-    ContactModel("Hurmat", "0707404370", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
-    ContactModel("M.Ali", "0772680138", false),
+class HomePage extends StatelessWidget {
+  List<DayInWeek> _days = [
+    DayInWeek(
+      "Sat",
+    ),
+    DayInWeek(
+      "Sun",
+    ),
+    DayInWeek(
+      "Mon",
+    ),
+    DayInWeek(
+      "Tue",
+    ),
+    DayInWeek(
+      "Wed",
+    ),
+    DayInWeek(
+      "Thu",
+    ),
+    DayInWeek(
+      "Fri",
+    ),
   ];
-
-  List<ContactModel> selectedContacts = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Multi Selection ListView"),
-        centerTitle: true,
-        backgroundColor: Colors.green[700],
+        title: Text("Select days in week"),
       ),
-      body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                    itemCount: contacts.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      // return item
-                      return ContactItem(
-                        contacts[index].name,
-                        contacts[index].phoneNumber,
-                        contacts[index].isSelected,
-                        index,
-                      );
-                    }),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SelectWeekDays(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              days: _days,
+              boxDecoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: primaryColor,
+                // gradient: LinearGradient(
+                //   begin: Alignment.topLeft,
+                //   // 10% of the width, so there are ten blinds.
+                //   colors: [
+                //     const Color(0xFFE55CE4),
+                //     const Color(0xFFBB75FB)
+                //   ], // whitish to gray
+                //   tileMode:
+                //       TileMode.repeated, // repeats the gradient over the canvas
+                // ),
               ),
-              selectedContacts.length > 0 ? Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 25,
-                  vertical: 10,
-                ),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: RaisedButton(
-                    color: Colors.green[700],
-                    child: Text(
-                      "Delete (${selectedContacts.length})",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                    onPressed: () {
-                      print("Delete List Lenght: ${selectedContacts.length}");
-                      for(var select in selectedContacts){
-                        print(select.name);
-                      }
-                    },
-                  ),
-                ),
-              ): Container(),
-            ],
+              onSelect: (values) {
+                log('$values');
+              },
+            ),
           ),
-        ),
+        ],
       ),
-    );
-  }
-
-  Widget ContactItem(
-      String name, String phoneNumber, bool isSelected, int index) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Colors.green[700],
-        child: Icon(
-          Icons.person_outline_outlined,
-          color: Colors.white,
-        ),
-      ),
-      title: Text(
-        name,
-        style: TextStyle(
-          fontWeight: FontWeight.w500,
-        ),
-      ),
-      subtitle: Text(phoneNumber),
-      trailing: isSelected
-          ? Icon(
-        Icons.check_circle,
-        color: primaryColor,
-      )
-          : Icon(
-        Icons.check_circle_outline,
-        color: Colors.grey,
-      ),
-      onTap: () {
-        setState(() {
-          contacts[index].isSelected = !contacts[index].isSelected;
-          if (contacts[index].isSelected == true) {
-            selectedContacts.add(ContactModel(name, phoneNumber, true));
-          } else if (contacts[index].isSelected == false) {
-            selectedContacts
-                .removeWhere((element) => element.name == contacts[index].name);
-          }
-        });
-      },
     );
   }
 }
-
-
