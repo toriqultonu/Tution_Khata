13133a2b8e5917fd3c612443452a40e9c7c74524
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-05-26 09:14:07 +0600

    updated

--- lib/views/attendance_list.dart
+++ lib/views/attendance_list.dart
@@ -17,13 +17,13 @@ class AttendanceList extends StatefulWidget {
 
 class _AttendanceListState extends State<AttendanceList> {
 
-  List<Attendance> selectedContacts = [];
-  List<Attendance> contacts = [];
+  List<Attendance> selectedStudents = [];
+  List<Attendance> students = [];
 
   @override
   void initState() {
     for(int i =0;i<widget.snapshot.data.length;i++){
-      contacts.add(Attendance(widget.snapshot.data[i].name, widget.snapshot.data[i].phone, widget.snapshot.data[i].id, false));
+      students.add(Attendance(widget.snapshot.data[i].name, widget.snapshot.data[i].phone, widget.snapshot.data[i].id, false));
     }
     super.initState();
   }
@@ -37,13 +37,13 @@ class _AttendanceListState extends State<AttendanceList> {
           ListView.builder(
               physics: ClampingScrollPhysics(),
               shrinkWrap: true,
-              itemCount: contacts.length,
+              itemCount: students.length,
               itemBuilder: (context, index) {
-                return ContactItem(
-                  contacts[index].name,
-                  contacts[index].phone,
-                  contacts[index].id,
-                  contacts[index].isSelected,
+                return AttendanceItem(
+                  students[index].name,
+                  students[index].phone,
+                  students[index].id,
+                  students[index].isSelected,
                   index,
                 );
               }
@@ -58,15 +58,15 @@ class _AttendanceListState extends State<AttendanceList> {
               child: RaisedButton(
                 color: primaryColor,
                 child: Text(
-                  "Submit (${selectedContacts.length})",
+                  "Submit (${selectedStudents.length})",
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
                   ),
                 ),
                 onPressed: () {
-                  print("Delete List Lenght: ${selectedContacts.length}");
-                  for(var select in selectedContacts){
+                  print("Delete List Lenght: ${selectedStudents.length}");
+                  for(var select in selectedStudents){
                     log('${select.name}');
                   }
                 },
@@ -80,7 +80,7 @@ class _AttendanceListState extends State<AttendanceList> {
   }
 
 
-  Widget ContactItem(
+  Widget AttendanceItem(
       String name, String phone, var id, bool isSelected, int index) {
     return ListTile(
       // leading: CircleAvatar(
@@ -108,12 +108,12 @@ class _AttendanceListState extends State<AttendanceList> {
       ),
       onTap: () {
         setState(() {
-          contacts[index].isSelected = !contacts[index].isSelected;
-          if (contacts[index].isSelected == true) {
-            selectedContacts.add(Attendance(name, phone, id,true));
-          } else if (contacts[index].isSelected == false) {
-            selectedContacts
-                .removeWhere((element) => element.id == contacts[index].id);
+          students[index].isSelected = !students[index].isSelected;
+          if (students[index].isSelected == true) {
+            selectedStudents.add(Attendance(name, phone, id,true));
+          } else if (students[index].isSelected == false) {
+            selectedStudents
+                .removeWhere((element) => element.id == students[index].id);
           }
         });
 
